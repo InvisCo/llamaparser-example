5a94989ad2bffe93de87e08f32f5fa582a6df80a
InvisCo <20621549+InvisCo@users.noreply.github.com>
2024-11-06 13:39:21 +0530

    Recurse queries

--- parser-qdrant-groq.py
+++ parser-qdrant-groq.py
@@ -14,7 +14,6 @@ from llama_parse import LlamaParse
 
 llamaparse_api_key = os.getenv("LLAMA_CLOUD_API_KEY")
 
-
 import pickle
 
 
@@ -42,15 +41,10 @@ def load_or_parse_data():
     return parsed_data
 
 
+print("Loading or parsing data...")
 # Call the function to either load or parse the data
 llama_parse_documents = load_or_parse_data()
-
-
-len(llama_parse_documents)
-
-llama_parse_documents[0].text[:100]
-
-type(llama_parse_documents)
+print("Done loading or parsing data.")
 
 ######## QDRANT ###########
 
@@ -67,6 +61,7 @@ qdrant_api_key = os.getenv("QDRANT_API_KEY")
 # by default llamaindex uses OpenAI models
 from llama_index.embeddings.fastembed import FastEmbedEmbedding
 
+print("Loading Embedding model...")
 embed_model = FastEmbedEmbedding(model_name="BAAI/bge-base-en-v1.5")
 
 """ embed_model = OllamaEmbedding(
@@ -75,7 +70,7 @@ embed_model = FastEmbedEmbedding(model_name="BAAI/bge-base-en-v1.5")
     base_url="http://localhost:11434",
     ollama_additional_kwargs={"mirostat": 0},
 ) """
-
+print("Done loading Embedding model.")
 #### Setting embed_model other than openAI ( by default used openAI's model)
 from llama_index.core import Settings
 
@@ -86,10 +81,10 @@ Settings.embed_model = embed_model
 from llama_index.llms.groq import Groq
 
 groq_api_key = os.getenv("GROQ_API_KEY")
-
+print("Loading Groq model...")
 llm = Groq(model="mixtral-8x7b-32768", api_key=groq_api_key)
 # llm = Groq(model="gemma-7b-it", api_key=groq_api_key)
-
+print("Done loading Groq model.")
 ######### Ollama ###########
 
 # from llama_index.llms.ollama import Ollama
@@ -97,12 +92,12 @@ llm = Groq(model="mixtral-8x7b-32768", api_key=groq_api_key)
 
 #### Setting llm other than openAI ( by default used openAI's model)
 Settings.llm = llm
-
+print("Connecting to Qdrant...")
 client = qdrant_client.QdrantClient(
     api_key=qdrant_api_key,
     url=qdrant_url,
 )
-
+print("Creating QdrantVectorStore...")
 vector_store = QdrantVectorStore(client=client, collection_name="qdrant_rag")
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 index = VectorStoreIndex.from_documents(
@@ -116,10 +111,26 @@ index = VectorStoreIndex.from_documents(
 # index = load_index_from_storage(storage_context)
 
 # create a query engine for the index
+print("Starting query engine...")
 query_engine = index.as_query_engine()
 
-# query the engine
-# query = "what is the common stock balance as of Balance as of March 31, 2022?"
-query = "what is the letter of credit As of December 31, 2021 "
-response = query_engine.query(query)
-print(response)
+print("====================================\n")
+
+while True:
+    # Prompt the user for input in the console
+    query = None
+    while not query:
+        query = input(
+            "What would you like to know about SaaS (Software as a Service)? (type 'thank you' to exit):\n >> "
+        )
+
+    # Check if the user wants to exit
+    if query.lower() == "thank you":
+        print("You're welcome!")
+        break
+
+    # Query the engine with the user's input
+    response = query_engine.query(query)
+
+    # Print the response
+    print(f"\n{response}\n")
